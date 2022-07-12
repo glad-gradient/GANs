1044a45f1a8ea8a1b5b03b66bef2ca072d8fde9b
glad-gradient <levitsky.serhii@gmaol.com>
2022-07-12 22:51:01 +0300

    Added nets

---
+++ nets.py
@@ -0,0 +1,17 @@
+import torch
+
+
+class Generator(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        pass
+
+    def forward(self, x):
+        pass
+
+
+class Discriminator(torch.nn.Module):
+    def __init__(self, in_features, out_features):
+        pass
+
+    def forward(self, x):
+        pass
